1afafee5b8aaa853186d0d2e06a5ea8395e0ca6f
Ted Semashov <tsemashov@amplify.com>
2022-04-27 22:11:41 +0200

    routes, controllers, middleware

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -7,7 +11,14 @@ dotenv.config({ path: './config/config.env' });
 // Create server
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5000;
 // Listen server
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
