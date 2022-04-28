c735b74fde9eed8a4f4130891ed784b783531874
Ted Semashov <tsemashov@amplify.com>
2022-04-28 22:01:43 +0200

    mongoose & global err handling

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to DB
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Create server
 const app = express();
@@ -21,4 +25,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 // Listen server
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.log(`Error: ${err.message}`);
+
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
