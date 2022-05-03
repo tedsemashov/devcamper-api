b8ef726fcbd92ce37864dba4e1f809bde5674caf
Ted Semashov <tsemashov@amplify.com>
2022-05-03 22:55:32 +0200

    CRUD operations, DB connect

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('colors');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
@@ -15,6 +16,9 @@ const bootcamps = require('./routes/bootcamps');
 // Create server
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -25,11 +29,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 // Listen server
-const server = app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.bold.red);
 
   // Close server & exit process
   server.close(() => process.exit(1));
