9a9dc095d25a72690a23ecf5887230f9dd19b8fc
Ted Semashov <tsemashov@amplify.com>
2022-05-16 22:00:47 +0200

    course schema, controller, seeder update, router GET

---
+++ controllers/courses.js
@@ -0,0 +1,25 @@
+const Course = require('../models/Course');
+// const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+
+// @desc    Get cources
+// @route   GET /api/v1/cources
+// @route   GET /api/v1/bootcamps/:bootcampId/courses
+// @access  Public
+exports.getCourses = asyncHandler(async (req, res) => {
+  let query;
+
+  if (req.params.bootcampId) {
+    query = Course.find({ bootcamp: req.params.bootcampId });
+  } else {
+    query = Course.find();
+  }
+
+  const cources = await query;
+
+  res.status(200).json({
+    success: true,
+    count: cources.length,
+    data: cources,
+  });
+});
--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -17,11 +18,13 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+const cources = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
 
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(cources);
 
     console.log('Data imported...'.green.inverse);
     process.exit();
@@ -34,6 +37,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany(); // will delete all data
+    await Course.deleteMany();
 
     console.log('Data destroyed...'.red.inverse);
     process.exit();
