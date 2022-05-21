4b02b8fc9067348fc8b10906fd8da9e597357479
Ted Semashov <tsemashov@amplify.com>
2022-05-21 16:37:55 +0200

    update, delete course

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,5 +1,6 @@
 const Course = require('../models/Course');
-// const ErrorResponse = require('../utils/errorResponse');
+const Bootcamp = require('../models/Bootcamp');
+const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 
 // @desc    Get cources
@@ -21,5 +22,83 @@ exports.getCourses = asyncHandler(async (req, res) => {
     success: true,
     count: cources.length,
     data: cources,
+  });
+});
+
+// @desc    Get single cources
+// @route   GET /api/v1/cources/:id
+// @access  Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name description',
+  });
+
+  if (!course) {
+    return next(new ErrorResponse(`No cource with the id of ${req.params.id}`), 404);
+  }
+
+  return res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc    Add cource
+// @route   POST /api/v1/bootcamps/:bootcampId/courses
+// @access  Private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`), 404);
+  }
+
+  const course = await Course.create(req.body);
+
+  return res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc    Update cource
+// @route   PUT /api/v1/courses/:id
+// @access  Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  return res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc    Delete cource
+// @route   DELETE /api/v1/courses/:id
+// @access  Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+  }
+
+  await course.remove();
+
+  return res.status(200).json({
+    success: true,
+    data: {},
   });
 });
