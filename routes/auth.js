dc5eacdfa42837e1361d9fe025e108ae8f1cf49d
Ted Semashov <tsemashov@amplify.com>
2022-06-16 20:34:56 +0200

    user update password + details

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {
-  register, login, getMe, forgotPassword, resetPassword,
+  register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword,
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -10,6 +10,8 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
